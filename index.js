0b90594e2a800d6b2e869200744c473a1474a97a
Marcin Swornowski <swor71@gmail.com>
2018-04-11 15:08:50 +0200

    Refactored the function

--- index.js
+++ index.js
@@ -1,16 +1,12 @@
 const formatDate = timeInSeconds => {
     if (timeInSeconds) {
-        let seconds = Math.floor(timeInSeconds);
-        let minutes = Math.floor(timeInSeconds / 60);
+        let seconds = Math.floor(timeInSeconds % 60);
+        let minutes = Math.floor(timeInSeconds % 3600 / 60);
         let hours = Math.floor(timeInSeconds / 3600);
 
-        if ( ( hours >= 1 ) && ( minutes % 60 == 0 ) && ( seconds % 60 == 0 ) ) return `${hours}h`;
-        if ( ( hours >= 1 ) && ( seconds % 60 == 0 ) ) return `${hours}h ${minutes % 60}m`
-        if ( ( hours >= 1 ) && ( minutes % 60 == 0 ) ) return `${hours}h ${seconds % 60}s`
-        if ( minutes >= 60 ) return `${hours}h ${minutes % 60}m ${seconds % 60}s`;
-        if ( ( seconds >= 60 ) && ( seconds % 60 == 0) ) return `${minutes}m`;
-        if ( seconds >= 60 ) return `${minutes}m ${seconds % 60}s`;
-        if ( seconds < 60 ) return `${seconds}s`;
+        const printTime = (time, unit) => time != 0 ? time + unit : ''
+
+        return `${printTime(hours, 'h')} ${printTime(minutes, 'm')} ${printTime(seconds, 's')}`.replace(/  /g, ' ').trim();
     }
     return `0s`;
 }
